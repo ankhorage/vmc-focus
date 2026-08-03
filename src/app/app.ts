c3edea1e380979f480cdd81df84154113d52305c
artiphishle <fabio.gartenmann@protonmail.com>
2026-08-03 14:44:21 +0200

    feat: add primary sidebar navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { SbbContainerModule } from '@sbb-esta/lyne-angular/container';
 import { SbbHeaderModule } from '@sbb-esta/lyne-angular/header';
+import { SbbSidebarModule } from '@sbb-esta/lyne-angular/sidebar';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterLink,
+    RouterLinkActive,
     RouterOutlet,
     SbbContainerModule,
     SbbHeaderModule,
+    SbbSidebarModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
