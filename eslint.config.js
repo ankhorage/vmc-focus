d2ad4c0e410c9e29cf5865667506f9b4221e3e11
artiphishle <fabio.gartenmann@protonmail.com>
2026-08-03 16:44:41 +0200

    chore: complete code quality tooling

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 const eslint = require('@eslint/js');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
 
 module.exports = defineConfig([
   {
@@ -38,4 +39,5 @@ module.exports = defineConfig([
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
     rules: {},
   },
+  eslintConfigPrettier,
 ]);
